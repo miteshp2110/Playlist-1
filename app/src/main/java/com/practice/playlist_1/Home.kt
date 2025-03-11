fe04a50f3f1d051b5da2672f00939ff15e2ffaee
sujal2617 <2200032771@kluniversity.in>
2025-03-12 01:18:55 +0530

    3nd

--- app/src/main/java/com/practice/playlist_1/Home.kt
+++ app/src/main/java/com/practice/playlist_1/Home.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        hideSystemBars()
+
+        val bottomnav=findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
+        loadFragment(HomeFragment())
+        bottomnav.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home->loadFragment(HomeFragment())
+                R.id.search->loadFragment(SearchFragment())
+                R.id.playlist->loadFragment(PlaylistFragment())
+                R.id.profile->loadFragment(ProfileFragment())
+            }
+            true
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
@@ -21,17 +33,9 @@ class Home : AppCompatActivity() {
         }
     }
 
-    private fun hideSystemBars() {
-        val controller = WindowInsetsControllerCompat(window, window.decorView)
-        controller.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        controller.hide(WindowInsetsCompat.Type.navigationBars())
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemBars()
-        }
+    private fun loadFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentloader,fragment)
+            .commit()
     }
 }
